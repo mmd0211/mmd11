05e149e7482179577f6a9b365a2544780ccc9a8e
mri <m.r.izady.1383@gmail.com>
2024-02-01 20:32:43 +0330

    changes

--- src/main.c
+++ src/main.c
@@ -11,14 +11,14 @@ int main()
 #else
 int main(int argc, char const *argv[])
 {
-#endif
-printf("Hello!\n");
-printf("The Number of arguments is : \"%d\" \n", argc);
-for (int i = 0; i < argc; i++)
-	printf("The Argument[%d] is : \"%s\" \n", i, argv[i]);
-printf("\n\n\n");
-printf("Is Exist = %d\n", is_exist(NULL));
+	printf("Hello!\n");
+	printf("The Number of arguments is : \"%d\" \n", argc);
+	for (int i = 0; i < argc; i++)
+		printf("The Argument[%d] is : \"%s\" \n", i, argv[i]);
+	printf("\n\n\n");
+	printf("Is Exist = %d\n", is_exist(NULL));
 
-printf("Extra Print\n");
-return 0;
+	printf("Extra Print\n");
+	return 0;
 }
+#endif
